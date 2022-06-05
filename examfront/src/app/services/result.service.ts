5bea0d8cc3bfdf8cfa67617e33d98ec7228c26c8
Vansh Verma <vanshvvvvv@gmail.com>
2022-06-05 13:57:27 +0530

    frontend result service added

---
+++ examfront/src/app/services/result.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResultService {
+
+  constructor(private _http:HttpClient) { }
+
+  public result(){
+    return this._http.get(`${baseUrl}/result/`)
+  }
+
+  public addResult(result:any){
+    return this._http.post(`${baseUrl}/result/`,result);
+  }
+
+  public getResultByUserAndQUiz(qid:any, uid:any){
+    return this._http.get(`${baseUrl}/result/${qid}/${uid}`);
+  }
+
+  public getResultByQuiz(qid:any){
+    return this._http.get(`${baseUrl}/result/${qid}`);
+  }
+}
